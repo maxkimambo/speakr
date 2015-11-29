0af4600f3ffd75cac2aac453804dde2f561e7ea3
Max Kimambo <max@kimambo.de>
2015-11-30 00:19:51 +0100

    added mock data to display upcoming events

--- server/api/home/index.js
+++ server/api/home/index.js
@@ -3,8 +3,19 @@ var router = express.Router();
 
 router.get('/', function(req, res, next){
 	
+	// mock data for now 
+	var upcomingTalks = [
+		{title: 'Introduction to AngularJS 2', summary: 'This presentation is supposed to give you basic concepts about using AngularJS 2', 
+		links: [{name: "Link name", url: "https://github.com/maxkimambo"}], presenters:[{name: 'Max Kimambo'}, {name: 'Andeas Harder'}],date: '26/11/2015'},
+		
+		{title: 'Introduction to NodeJS', summary: 'Discover how to build webapps with NodeJS', 
+		links: [{name: "Link name", url: "https://github.com/maxkimambo"}], 
+		presenters:[{name: 'Max Kimambo'}, {name: 'Andeas Harder'}], date: '26/01/2016'}]; 
+	
+	
 	res.render('home', {
-		title: 'Speakr'
+		title: 'Speakr',
+		events: upcomingTalks
 	}); 
 }); 
 
